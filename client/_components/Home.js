c6f5f5f24a5bcb7e21102e050c626b04f00ea1f5
The Iliad <iliadmoosavi@gmail.com>
2017-11-02 14:08:26 -0400

    Show list of repos

--- client/_components/Home.js
+++ client/_components/Home.js
@@ -16,37 +16,50 @@ import Subheader from 'material-ui/Subheader';
 import Divider from 'material-ui/Divider';
 import INFO_ICON from 'material-ui/svg-icons/action/info';
 
+//Axios
+import axios from 'axios';
+
 // Utilities
 import USERS_LIST from '../_utils/users';
 
 export default class Home extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            repos: []
+        };
+    }
+
+    componentWillMount() {
+        axios.get(`https://api.github.com/orgs/GopherLabsLtd/repos`)
+        .then(function (response) {
+          if (response.data && response.data.length > 0) {
+            this.setState({
+                repos: response.data
+            });
+          }
+        }.bind(this))
     }
     
     render() {
         const marginRight = { marginRight: "10px" }
         return (
             <div>
-                <h1 id="title">
-                    App Works!
-                </h1>
-
                 <div style={{
                 boxShadow: "0 5px 35px -2px rgba(0, 0, 0, 0.1)",
                 borderRadius: "2px",
                 marginBottom: "30px"
             }}>
                     <List>
-                        <Subheader>Recent chats</Subheader>
+                        <Subheader>Latest Repos</Subheader>
 
-                        {USERS_LIST.map((user, i) => {
+                        {this.state.repos !== [] && this.state.repos.map((repo, i) => {
                             return(
                                 <Link to={`/userDetails/${i}`}>
                                     <ListItem
-                                        primaryText={user.name.first + " " + user.name.last}
-                                        leftAvatar={<Avatar src={user.picture.large} />}
-                                        rightIcon={<INFO_ICON />}
+                                        primaryText={repo.name}
+                                        secondaryText={repo.description}
                                     />
                                 </Link>
                             )
